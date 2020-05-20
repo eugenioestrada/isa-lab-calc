01060f1658923744e73af2a676380a31aa3f28f3
Eugenio Estrada <eugenio.estrada@gmail.com>
2020-05-20 15:09:51 +0200

    Todos los tests en la misma clase

--- Calc.Tests/CalcServiceTests.cs
+++ Calc.Tests/CalcServiceTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Calc.Tests
 {
-    public class SumarTests
+    public class CalcServiceTests
     {
         [Theory]
         [InlineData(1, 1, 2)]
@@ -16,5 +16,19 @@ namespace Calc.Tests
             int resultado = calc.Sumar(operando1, operando2);
             Assert.Equal(resultado, resultadoEsperado);
         }
+
+        [Theory]
+        [InlineData(1, 1, 0)]
+        [InlineData(2, -2, 4)]
+        [InlineData(-10, 2, -12)]
+        [InlineData(-8, -4, -4)]
+        public void RestarTest(int operando1, int operando2, int resultadoEsperado)
+        {
+            var calc = new Calc.CalcService();
+            int resultado = calc.Restar(operando1, operando2);
+            Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        
     }
 }
