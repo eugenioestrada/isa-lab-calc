ed30e34c59f19f6146fee47f3b9b40b6cfcfe46d
Eugenio Estrada <eugenio.estrada@gmail.com>
2020-05-20 15:05:00 +0200

    Añadidos más test a la suma

--- Calc.Tests/SumarTests.cs
+++ Calc.Tests/SumarTests.cs
@@ -5,13 +5,13 @@ namespace Calc.Tests
 {
     public class SumarTests
     {
-        [Fact]
-        public void SumarTest()
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, -2, 0)]
+        [InlineData(-10, 2, -8)]
+        [InlineData(-8, -4, -12)]
+        public void SumarTest(int operando1, int operando2, int resultadoEsperado)
         {
-            int operando1 = 1;
-            int operando2 = 1;
-            int resultadoEsperado = 2;
-
             var calc = new Calc.CalcService();
             int resultado = calc.Sumar(operando1, operando2);
             Assert.Equal(resultado, resultadoEsperado);
