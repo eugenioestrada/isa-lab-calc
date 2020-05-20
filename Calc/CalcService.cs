812d5a35b2f5fe2ce0c9eb2be876a63235f4be52
Eugenio Estrada <eugenio.estrada@gmail.com>
2020-05-20 15:02:21 +0200

    implementado el método sumar

--- Calc/CalcService.cs
+++ Calc/CalcService.cs
@@ -4,5 +4,9 @@ namespace Calc
 {
     public class CalcService
     {
+        public int Sumar(int operando1, int operando2)
+        {
+            return operando1 + operando2;
+        }
     }
 }
